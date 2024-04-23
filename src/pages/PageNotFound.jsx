824d5cb3d990e550f5146317cae588b40869df88
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-04-23 22:49:34 +0200

    Linking Between Routes with Link and NavLink done

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,12 @@
 // import React from "react";
 
+import PageNav from "../components/PageNav";
+
 export default function PageNotFound() {
-    return <div>404 Not Found :(</div>;
+    return (
+        <div>
+            <PageNav />
+            404 Not Found :(
+        </div>
+    );
 }
