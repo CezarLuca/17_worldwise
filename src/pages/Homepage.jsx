0c097835fc0756ac2ad1df412d27014537652934
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-04-25 23:58:53 +0200

    Importing the starter files

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,13 @@
 // import React from 'react'
 import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 export default function Homepage() {
     return (
         <div>
             <PageNav />
+            <AppNav />
             <h1>Worldwise</h1>
             <Link to="/app">Got to the app</Link>
 
