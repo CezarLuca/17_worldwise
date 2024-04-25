7c91089dc06de65062f40b339f5a29e9ef15cedb
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-04-26 00:32:33 +0200

    Using CSS Modules done

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -8,7 +8,7 @@ export default function Homepage() {
         <div>
             <PageNav />
             <AppNav />
-            <h1>Worldwise</h1>
+            <h1 className="test">Worldwise</h1>
             <Link to="/app">Got to the app</Link>
 
             {/* <a href="/pricing">Pricing</a> */}
