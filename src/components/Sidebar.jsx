91b35d4e202f40967b7fba14656787373bea0aa4
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-04-29 23:39:42 +0200

    Building the App Layout part 2

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,11 @@ export default function Sidebar() {
             <AppNav />
 
             <p>List of cities</p>
-            <footer className=""></footer>
+            <footer className={styles.footer}>
+                <p className={styles.copyright}>
+                    &copy; Copyright {new Date().getFullYear()} by WorldWise
+                </p>
+            </footer>
         </div>
     );
 }
