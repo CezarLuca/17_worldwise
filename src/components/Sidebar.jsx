7a9df4600a0a65fee08a52330672984de2dc0fcf
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-04-30 16:42:41 +0200

    Added Footer Component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import AppNav from "./AppNav";
 import Logo from "./Logo";
+import Footer from "./Footer";
 import styles from "./Sidebar.module.css";
 
 export default function Sidebar() {
@@ -7,13 +8,8 @@ export default function Sidebar() {
         <div className={styles.sidebar}>
             <Logo />
             <AppNav />
-
             <p>List of cities</p>
-            <footer className={styles.footer}>
-                <p className={styles.copyright}>
-                    &copy; Copyright {new Date().getFullYear()} by WorldWise
-                </p>
-            </footer>
+            <Footer />
         </div>
     );
 }
