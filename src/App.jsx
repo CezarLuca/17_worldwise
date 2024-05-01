234e76922213e1499602eaed359356aa64662a45
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-05-02 00:04:24 +0200

    Implementing the CityList component part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Login from "./pages/Login";
@@ -8,7 +9,28 @@ import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import CityList from "./components/CityList";
 
+const BASE_URL = "http://localhost:3001";
+
 export default function App() {
+    const [cities, setCities] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        async function fetchCities() {
+            try {
+                setIsLoading(true);
+                const res = await fetch(`${BASE_URL}/cities`);
+                const data = await res.json();
+                setCities(data);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        fetchCities();
+    }, []);
+
     return (
         <BrowserRouter>
             <Routes>
