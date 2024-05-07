c80f6463cdb9cf9bc7e678d3f6282bd632eadc71
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-05-07 13:52:27 +0200

    Added some neat navigation aid, might be replaced in the tutorial

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import styles from "./CityItem.module.css";
 import PropTypes from "prop-types";
 
@@ -13,11 +13,19 @@ function CityItem({ city }) {
     const { cityName, date, id, position } = city;
     // console.log("CityItem", position);
     const emoji = city.emoji || "🏙️"; // Default emoji if none is provided
+    const linkLocation = useLocation();
+    let linkPath = `${id}?lat=${position.lat}&lng=&${position.lng}`;
+
+    if (linkLocation.pathname === "/app") {
+        linkPath = "/app/cities/" + linkPath;
+    }
+
     return (
         <li>
             <Link
                 className={styles.cityItem}
-                to={`${id}?lat=${position.lat}&lng=&${position.lng}`}
+                // to={`${id}?lat=${position.lat}&lng=&${position.lng}`}
+                to={linkPath}
             >
                 <span className={styles.emoji}>{emoji}</span>
                 <h3 className={styles.name}>{cityName}</h3>
