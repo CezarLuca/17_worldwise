522d089a577b289d2778387da7b3de5c5302d25d
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-05-23 23:59:48 +0200

    part 3

--- src/AppOld.jsx
+++ src/AppOld.jsx
@@ -1,4 +1,6 @@
+// import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import { useEffect, useState } from "react";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Login from "./pages/Login";
@@ -10,7 +12,28 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import Form from "./components/Form";
 
+// const BASE_URL = "http://localhost:3001";
+
 export default function App() {
+    // const [cities, setCities] = useState([]);
+    // const [isLoading, setIsLoading] = useState(false);
+
+    // useEffect(() => {
+    //     async function fetchCities() {
+    //         try {
+    //             setIsLoading(true);
+    //             const res = await fetch(`${BASE_URL}/cities`);
+    //             const data = await res.json();
+    //             setCities(data);
+    //         } catch (error) {
+    //             console.error(error);
+    //         } finally {
+    //             setIsLoading(false);
+    //         }
+    //     }
+    //     fetchCities();
+    // }, []);
+
     return (
         <BrowserRouter>
             <Routes>
