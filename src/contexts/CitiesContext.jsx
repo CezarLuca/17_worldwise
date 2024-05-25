a660f5f343f6c536043bbe0b771c7aee67a6fcf7
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-05-25 23:58:07 +0200

    Creating CitiesContext done

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -23,4 +23,11 @@ function CitiesProvider({ children }) {
         }
         fetchCities();
     }, []);
+    return (
+        <CitiesContext.Provider value={{ cities, isLoading }}>
+            {children}
+        </CitiesContext.Provider>
+    );
 }
+
+export { CitiesProvider, CitiesContext };
