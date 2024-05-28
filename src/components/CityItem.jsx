cb31ebcfba6751fc9a232b17d6fc7c208ec3f305
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-05-28 23:59:48 +0200

    Finishing the City View part 3

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,6 +1,7 @@
 import { Link, useLocation } from "react-router-dom";
 import styles from "./CityItem.module.css";
 import PropTypes from "prop-types";
+import { useCities } from "../contexts/CitiesContext";
 
 const formatDate = (date) =>
     new Intl.DateTimeFormat("en", {
@@ -10,6 +11,7 @@ const formatDate = (date) =>
     }).format(new Date(date));
 
 function CityItem({ city }) {
+    const { currentCity } = useCities();
     const { cityName, date, id, position } = city;
     // console.log("CityItem", position);
     const emoji = city.emoji || "🏙️"; // Default emoji if none is provided
@@ -23,7 +25,9 @@ function CityItem({ city }) {
     return (
         <li>
             <Link
-                className={styles.cityItem}
+                className={`${styles.cityItem} ${
+                    id === currentCity.id ? styles["cityItem--active"] : ""
+                } `}
                 // to={`${id}?lat=${position.lat}&lng=&${position.lng}`}
                 to={linkPath}
             >
