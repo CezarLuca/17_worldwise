637be35a376faa390fcb60eb9a9c5eb3f8d1c596
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-06-01 22:51:15 +0200

    Icluding a Map with the Leaflet Library done

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -33,9 +33,10 @@ export default function Map() {
                 center={mapPosition}
                 zoom={13}
                 scrollWheelZoom={false}
+                className={styles.map}
             >
                 <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
                 <Marker position={mapPosition}>
