eca2ba8f09f143538802625957f68a7b68fe091d
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-06-04 23:59:46 +0200

    Adding Click to Open Form Functionality

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,19 @@
 import PropTypes from "prop-types";
 import { useNavigate, useSearchParams } from "react-router-dom";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import {
+    MapContainer,
+    TileLayer,
+    Marker,
+    Popup,
+    useMap,
+    useMapEvents,
+} from "react-leaflet";
 import styles from "./Map.module.css";
 import { useEffect, useState } from "react";
 import { useCities } from "../hooks/useCities";
 
 export default function Map() {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
     const { citites } = useCities();
 
     const [mapPosition, setMapPosition] = useState([51.505, -0.09]);
@@ -62,6 +69,7 @@ export default function Map() {
                     </Marker>
                 ))}
                 <ChangeCenter position={mapPosition} />
+                <DetectClick />
             </MapContainer>
         </div>
     );
@@ -71,6 +79,17 @@ function ChangeCenter({ position }) {
     const map = useMap();
     map.setView(position);
     return null;
+}
+
+function DetectClick() {
+    const navigate = useNavigate();
+
+    useMapEvents({
+        click: (e) => {
+            navigate(`/form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+            // console.log(e.latlng);
+        },
+    });
 }
 
 ChangeCenter.propTypes = {
