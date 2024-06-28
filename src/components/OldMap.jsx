ca2a47c50511498ad2114c5c14acddd680e7c763
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-06-28 23:51:40 +0200

    Cleaned the Map component moved to OldMap component

--- src/components/OldMap.jsx
+++ src/components/OldMap.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+// import { useNavigate, useSearchParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import {
     MapContainer,
@@ -16,6 +17,7 @@ import AppButton from "./AppButton";
 import { useUrlPosition } from "../hooks/useUrlPosition";
 
 export default function Map() {
+    // const navigate = useNavigate();
     const { cities } = useCities();
     const [mapPosition, setMapPosition] = useState([51.505, -0.09]);
     const {
@@ -24,6 +26,10 @@ export default function Map() {
         getPosition,
     } = useGeolocation();
     const [mapLat, mapLng] = useUrlPosition();
+
+    // const [searchParams] = useSearchParams();
+    // const mapLat = searchParams.get("lat");
+    // const mapLng = searchParams.get("lng");
 
     useEffect(() => {
         if (mapLat && mapLng) {
@@ -41,14 +47,31 @@ export default function Map() {
     }, [geolocationPosition]);
 
     return (
-        <div className={styles.mapContainer}>
+        <div
+            className={styles.mapContainer}
+            // onClick={() => {
+            //     navigate("form");
+            // }}
+        >
             {!geolocationPosition && (
                 <AppButton type="positon" onClick={() => getPosition()}>
                     {isLoadingPosition ? "Loading..." : "Get Position"}
                 </AppButton>
             )}
+            {/* <h1>Map</h1>
+            <h2>
+                Position: {lat} {lng}
+            </h2>
+            <button
+                onClick={() => {
+                    setSearchParams({ lat: 23, lng: 50 });
+                }}
+            >
+                Change Position
+            </button> */}
             <MapContainer
                 center={mapPosition}
+                // center={[mapLat || 40, mapLng || 0]}
                 zoom={8}
                 scrollWheelZoom={false}
                 className={styles.map}
@@ -88,6 +111,8 @@ function DetectClick() {
     useMapEvents({
         click: (e) => {
             navigate(`/app/form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+            // navigate(`form`);
+            // console.log(e.latlng);
         },
     });
 }
