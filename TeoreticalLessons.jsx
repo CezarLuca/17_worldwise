900a67a93bc48c611f337c385c85fad364f63339
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-07-16 23:59:24 +0200

    Teoretical Lesson useEffect dependency array rules

--- TeoreticalLessons.jsx
+++ TeoreticalLessons.jsx
@@ -1,3 +1,14 @@
 /*
-    Consuming Contexrt in a class component
+    UseEffect Dependency Array Rules:
+    
+    1. Every state variable, prop, and context value used inside the effect must 
+        be included in the dependency array.
+    
+    2. All "reactive" values should be included. That means any function or variable
+        that reference any other reactive value.
+    
+    3. Dependencies choose themselves: Never ignore the exhaustive-deps ESLint warning.
+
+    4. Do not use object or arrays as dependecies (objects are recreated on each render,
+        and React sees new objects as different, {} !== {}).
 */
